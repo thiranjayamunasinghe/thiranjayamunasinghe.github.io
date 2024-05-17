b6bfe4c6d67db7b0de3284e69a2b68010f2ef998
Thiranjaya <thiranjayamunasinghe@gmail.com>
2024-05-18 01:58:22 +0530

    updated site

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <div className="flex flex-shrink-0 items-center">
           <Link
             href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
+            className=" text-[#16f2b3] text-xl font-bold">
             THIRANJAYA MUNASINGHE
           </Link>
         </div>
